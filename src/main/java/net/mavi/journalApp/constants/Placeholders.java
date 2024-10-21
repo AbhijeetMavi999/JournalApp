121a898f7bc735ab772994f6fd831b744b7ee049
Abhijeet Mavi <abhijeetmavi999@gmail.com>
2024-10-21 10:49:55 +0530

    kafka implemented

---
+++ src/main/java/net/mavi/journalApp/constants/Placeholders.java
@@ -0,0 +1,6 @@
+package net.mavi.journalApp.constants;
+
+public interface Placeholders {
+    String API_KEY = "<apiKey>";
+    String CITY = "<city>";
+}
---
+++ src/main/java/net/mavi/journalApp/repository/ConfigJournalAppRepository.java
@@ -0,0 +1,9 @@
+package net.mavi.journalApp.repository;
+
+import net.mavi.journalApp.entity.ConfigJournalAppEntity;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ConfigJournalAppRepository extends MongoRepository<ConfigJournalAppEntity, ObjectId> {
+
+}
